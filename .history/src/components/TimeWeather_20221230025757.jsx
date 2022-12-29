2a9b389c1fcdbff280ccd193f2f959cef530f315
Songeun <songsilver.lee@gmail.com>
2022-12-30 03:15:25 +0900

    :lipstick: modify weatherDetail ui
    modify weatherDetail ui

---
+++ .history/src/components/TimeWeather_20221230025757.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { weatherIcon, weatherImg } from "../util/getValue";
+
+import Clothes from "./Clothes";
+export default function TimeWeather({ data }) {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate(`/weatherdetail/${data[0].fcstDate}${data[0].fcstTime}`, {
+      state: data,
+    });
+  };
+  return (
+    <section
+      onClick={handleClick}
+      className="flex bg-color3 rounded-lg mb-3 p-2 items-center cursor-pointer hover:ease-in duration-200 hover:scale-102"
+    >
+      <div className=" bg-color1 rounded-lg  items-center w-1/3 p-1">
+        <div className="flex items-center justify-center ">
+          <div className="w-8 pr-2">
+            {weatherImg(
+              data[0].fcstTime,
+              parseInt(0),
+
+              parseInt(2)
+            )}
+          </div>
+          <div>{data[0].fcstValue}°</div>
+        </div>
+        <div className="flex justify-center">{time(data[0].fcstTime)}</div>
+      </div>
+      <Clothes temperature={data[0].fcstValue} />
+    </section>
+  );
+}
+
+function time(fcstTime) {
+  const time = `${fcstTime.substr(0, 2)}:${fcstTime.substr(2, 4)}`;
+  return time;
+}
---
+++ .history/src/pages/WeatherDetail_20221230024733.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+import ClothesDetail from "../components/ClothesDetail";
+import WeatherInfoDetail from "../components/WeatherInfoDetail";
+
+export default function WeatherDetail() {
+  const location = useLocation();
+  const weatherInfo = location.state;
+  return (
+    <section className="flex flex-col items-center p-2 font-serif">
+      <WeatherInfoDetail data={weatherInfo} />
+      <ClothesDetail temperature={weatherInfo[0].fcstValue} />
+    </section>
+  );
+}
