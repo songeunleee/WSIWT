c7873415fcd7a7d22254dafb39580f0919b0553d
Songeun <songsilver.lee@gmail.com>
2023-01-02 05:21:12 +0900

    :lipstick: add login ui
    add navbat button, myCloset ui

---
+++ .history/src/pages/WeatherDetail_20230102042156.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+import ClosetClothes from "../components/ClosetClothes";
+import ClothesDetail from "../components/ClothesDetail";
+import WeatherInfoDetail from "../components/WeatherInfoDetail";
+
+export default function WeatherDetail() {
+  const location = useLocation();
+  const weatherInfo = location.state;
+  return (
+    <section className="flex flex-col items-center p-2">
+      <WeatherInfoDetail data={weatherInfo} />
+      <ClothesDetail temperature={weatherInfo[0].fcstValue} />
+      <div>
+        <ClosetClothes />
+      </div>
+    </section>
+  );
+}
